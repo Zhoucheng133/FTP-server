afaf4a432e9df6c08e3e891fe5178d1dbec24da4
zhoucheng <zhoucheng133@outlook.com>
2023-12-07 20:54:01 +0800

    使用Fluent UI

--- main.py
+++ main.py
@@ -1,76 +1,61 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QWidget, QHBoxLayout, QSizePolicy
-from PyQt6.QtGui import QPixmap, QMouseEvent
-from PyQt6.QtCore import Qt, QPoint, QPointF
+# coding:utf-8
+import sys
 
-class MainWindow(QMainWindow):
+from PyQt6.QtCore import Qt, QUrl
+from PyQt6.QtGui import QIcon, QDesktopServices
+from PyQt6.QtWidgets import QApplication, QFrame, QHBoxLayout
+from qfluentwidgets import (NavigationItemPosition, MessageBox, setTheme, Theme, FluentWindow,
+                            NavigationAvatarWidget, qrouter, SubtitleLabel, setFont, InfoBadge,
+                            InfoBadgePosition)
+from qfluentwidgets import FluentIcon as FIF
+
+
+class Widget(QFrame):
+
+    def __init__(self, text: str, parent=None):
+        super().__init__(parent=parent)
+        self.label = SubtitleLabel(text, self)
+        self.hBoxLayout = QHBoxLayout(self)
+
+        setFont(self.label, 24)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.hBoxLayout.addWidget(self.label, 1, Qt.AlignmentFlag.AlignCenter)
+        self.setObjectName(text.replace(' ', '-'))
+
+
+class Window(FluentWindow):
+
     def __init__(self):
         super().__init__()
 
-        # 窗口内容
-        main_widget = QWidget()
-        self.setCentralWidget(main_widget)
-        self.setup_layout()
+        # create sub interface
+        self.homeInterface = Widget('主页', self)
+        self.infoInterface = Widget('关于', self)
 
-        # 窗口属性
-        self.setFixedSize(400, 500)
-        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
-        self.setStyleSheet("background-color: white;")
-    
-    def mousePressEvent(self, event: QMouseEvent):
-        self.drag_start_position = event.globalPosition()
+        self.initNavigation()
+        self.initWindow()
 
-    def mouseMoveEvent(self, event: QMouseEvent):
-        delta = event.globalPosition() - self.drag_start_position
-        delta_pointf = QPointF(delta)
-        self.move(self.pos() + delta_pointf.toPoint())
-        self.drag_start_position = event.globalPosition()
+    def initNavigation(self):
+        self.addSubInterface(self.homeInterface, FIF.HOME, '主页')
+        self.addSubInterface(self.infoInterface, FIF.INFO, '关于')
 
-    def titleBar(self):
-        titleBarContent = QHBoxLayout()
-        titleBarContent.setSpacing(0)
-        titleBarContent.setAlignment(Qt.AlignmentFlag.AlignRight)
+        # NOTE: enable acrylic effect
+        # self.navigationInterface.setAcrylicEnabled(True)
 
-        # 可拖动区域
-        dragArea=QLabel(self)
-        dragArea.setFixedSize(340,30)
-        self.drag_start_position = QPoint(0, 0)
-        # 信号连接到事件处理函数
-        dragArea.mousePressEvent = self.mousePressEvent
-        dragArea.mouseMoveEvent = self.mouseMoveEvent
-        # dragArea.mouseReleaseEvent = self.mouseReleaseEvent
+    def initWindow(self):
+        self.resize(900, 700)
+        self.setWindowIcon(QIcon(':/qfluentwidgets/images/logo.png'))
+        self.setWindowTitle('PyQt-Fluent-Widgets')
 
-        # 最小化按钮
-        minButton=QLabel(self)
-        pixmap = QPixmap("assets/minus.svg")
-        minButton.setPixmap(pixmap)
-        minButton.setFixedSize(30,30)
-        minButton.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        minButton.setStyleSheet("font-size: 16pt;")
-
-        # 关闭按钮
-        closeButton=QLabel(self)
-        pixmap = QPixmap("assets/close.svg")
-        closeButton.setPixmap(pixmap)
-        closeButton.setFixedSize(30,30)
-        closeButton.setStyleSheet("background-color: red; font-size: 12pt; color: white")
-
-        closeButton.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        
-        titleBarContent.addWidget(dragArea)
-        titleBarContent.addWidget(minButton)
-        titleBarContent.addWidget(closeButton)
-        return titleBarContent
+        desktop = QApplication.screens()[0].availableGeometry()
+        w, h = desktop.width(), desktop.height()
+        self.move(w//2 - self.width()//2, h//2 - self.height()//2)
 
 
-    def setup_layout(self):
-        vertical_layout = QVBoxLayout(self.centralWidget())
-        vertical_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
-        vertical_layout.setContentsMargins(0, 0, 0, 0)
-        # 将两个水平布局添加到垂直布局中
-        vertical_layout.addLayout(self.titleBar())
+if __name__ == '__main__':
+    # setTheme(Theme.DARK)
 
-if __name__ == "__main__":
-    app = QApplication([])
-    main_window = MainWindow()
-    main_window.show()
+    app = QApplication(sys.argv)
+    w = Window()
+    w.show()
     app.exec()
