3afa1d138773f82c0fcf79f5d547c0ec1fd33ebe
zhoucheng <zhoucheng133@outlook.com>
2023-12-09 12:44:21 +0800

    完成左半边layout

--- views/home.py
+++ views/home.py
@@ -1,7 +1,7 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QApplication, QFrame, QHBoxLayout, QVBoxLayout
-from qfluentwidgets import (FluentWindow, SubtitleLabel, setFont, TogglePushButton)
+from PyQt6.QtWidgets import QApplication, QFrame, QHBoxLayout, QVBoxLayout, QSpacerItem, QSizePolicy, QLabel
+from qfluentwidgets import (FluentWindow, SubtitleLabel, setFont, TogglePushButton, LineEdit, PushButton, SpinBox)
 from qfluentwidgets import FluentIcon as FIF
 
 class homeInterface(QFrame):
@@ -9,8 +9,55 @@ class homeInterface(QFrame):
         super().__init__(parent=parent)
         self.vBoxLayout = QVBoxLayout(self)
         horizontal_layout = QHBoxLayout()
+        horizontal_layout.setContentsMargins(20, 20, 20, 20)
+
+        # 左侧布局
+        leftSide=QVBoxLayout()
+
+        # 路径选择
+        pathSelecter=QHBoxLayout()
+        # 路径输入框
+        pathInput=LineEdit()
+        pathInput.setEnabled(False)
+        pathInput.setText("hello")
+        # 路径浏览按钮
+        pathSelectButton=PushButton("浏览")
+        # 添加到路径选择
+        pathSelecter.addWidget(pathInput)
+        pathSelecter.addWidget(pathSelectButton)
+
+        # 端口选择
+        portSelector=QHBoxLayout()
+        # 端口选择文本
+        portLabel=QLabel("FTP服务端口:")
+        portLabel.setStyleSheet("font-size: 15px")
+        # 端口选择框
+        portInput=SpinBox()
+        portInput.setValue(21)
+        portInput.setFixedWidth(140)
+        # 添加到端口选择
+        portSelector.addWidget(portLabel)
+        portSelector.addWidget(portInput)
+        portSelector.setAlignment(Qt.AlignmentFlag.AlignLeft)
+
+        leftSide.addLayout(pathSelecter)
+        leftSide.addLayout(portSelector)
+
+        rightSide=QVBoxLayout()
+
+        horizontal_layout.addLayout(leftSide)
+        horizontal_layout.addLayout(rightSide)
+
+        spacer_item = QSpacerItem(0, 0, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        horizontal_layout.addSpacerItem(spacer_item)
+
+        # 运行按钮
+        runbuttonLayout=QHBoxLayout()
         runButton = TogglePushButton("启动服务")
+        runButton.setFixedWidth(100)
+        runbuttonLayout.addWidget(runButton)
+        runbuttonLayout.setAlignment(Qt.AlignmentFlag.AlignHCenter)
 
         self.vBoxLayout.addLayout(horizontal_layout)
-        self.vBoxLayout.addWidget(runButton)
+        self.vBoxLayout.addLayout(runbuttonLayout)
         self.setObjectName("homeView")
